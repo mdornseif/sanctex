4bec87b1a67df27611c87c1f40626b4127348af4
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2010-11-12 14:58:00 +0200

    Don't mask exceptions in settings.py

--- manage.py
+++ manage.py
@@ -4,13 +4,8 @@
 # created november 2009 by danielroseman for Hudora GmbH
 
 from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    print sys.path
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    raise
+
+import settings # Assumed to be in the same directory.
 
 if __name__ == "__main__":
     execute_manager(settings)
