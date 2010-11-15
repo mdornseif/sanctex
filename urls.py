08a0a60ec72ce6c9213d245b9e3faea8e9db01bd
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2010-11-15 23:42:09 +0200

    Remove django static server

--- urls.py
+++ urls.py
@@ -2,6 +2,11 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 from sanctions.models import Entity
 
+
+import dbindexer
+dbindexer.autodiscover()
+
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -15,14 +20,10 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     url(r'download/$', 'sanctions.views.download', name='download'),
     (r'api/', include('api.urls')),
-    
+
     (r'^entity/(?P<object_id>\d+)/', 'django.views.generic.list_detail.object_detail', info_dict),
     (r'^search/', 'sanctions.views.search'),
     (r'^hintergrund/', 'django.views.generic.simple.direct_to_template', {'template': 'sanctions/hintergrund.html'}),
     (r'^how/', 'django.views.generic.simple.direct_to_template', {'template': 'sanctions/how.html'}),
     (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'sanctions/index.html'}),
-    
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-         'document_root': settings.OUR_ROOT + '/media' }
-     ),
 )
