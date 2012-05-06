17334e4cdcfe750b73ad9d6461665ad0543283c2
Maximillian Dornseif <md@hudora.de>
2012-05-06 22:01:15 +0200

    huTools als modernes Submodul für die Tests

--- config.py
+++ config.py
@@ -7,39 +7,37 @@ Created by Maximillian Dornseif on 2010-09-28.
 Copyright (c) 2010 HUDORA. All rights reserved.
 """
 
-import sys
+# pylint: disable=C0103
+
+# importing lib automatically extends sys.path
+import lib
+lib.imported = True
+
 import os
-
-newpath = []
-_basedir = os.path.dirname(__file__)
-libdir = os.path.join(_basedir, 'pythonenv/lib/python2.5/site-packages/')
-if os.path.isdir(libdir):
-    newpath.append(libdir)
-    # appengine doesn't support '.egg-link' links created by pip
-    newpath.extend([os.path.join(_basedir,
-                        'pythonenv',
-                        open(os.path.join(libdir, n)).read().strip('\n.').split('pythonenv')[1])
-                for n in os.listdir(libdir) if n.endswith('.egg-link')])
-
-# add all subdirs in ./lib
-submoduledir = os.path.join(_basedir, 'lib')
-newpath.extend([os.path.join(submoduledir, n)
-            for n in os.listdir(submoduledir)
-            if os.path.isdir(os.path.join(submoduledir, n))])
-
-sys.path = newpath + sys.path
-
-#from google.appengine.dist import use_library
-#use_library('django', '1.1')
-#import django
 
 template_dirs = []
 template_dirs.append(os.path.join(os.path.dirname(__file__), 'templates'))
 
 
+import logging
+import warnings
+
+
+# Die Parameternamen sind vorgegeben, da können wir nichts verbessern.
+# pylint: disable=W0613,W0622,R0913
+def _customwarn(message, category, filename, lineno, file=None, line=None):
+    """Ensure that warning-messages go to the AppEngine log"""
+    logging.warn(warnings.formatwarning(message, category, filename, lineno))
+# pylint: enable=W0613,W0622,R0913
+
+
+warnings.showwarning = _customwarn
+warnings.filterwarnings("once")
+
 def main():
     """show path for usage in scripts"""
-    print ':'.join(newpath)
+    import sys
+    print ':'.join(sys.path)
 
 if __name__ == '__main__':
     main()
